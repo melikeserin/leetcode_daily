64d7fb6293e97e17c461d3c5351922e9360fedda
melikeserin <139703192+melikeserin@users.noreply.github.com>
2025-06-05 23:27:55 +0300

    Create problem14.java

---
+++ problem14.java
@@ -0,0 +1,14 @@
+class Solution{
+  public String longestCommonPrefix(String [] strs) {
+    if (strs.length == 0 || strs == null) {
+      return "";
+    }
+    for (int i==; i<strs[0].length(); i++){
+      char ch = strs[0].charAt(i);
+      for (int j=1; j<strs.length() || strs[j].charAt(i) != ch){
+         return strs [0].substring(0, i);
+      }
+    }
+    return strs[0];
+  }
+}
